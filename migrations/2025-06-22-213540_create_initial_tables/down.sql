975eceaa5df5ef7c4162eb3499ee4139d99e4d77
nareph <narephfrank@gmail.com>
2025-07-20 01:31:04 +0100

    Update down.sql

--- migrations/2025-06-22-213540_create_initial_tables/down.sql
+++ migrations/2025-06-22-213540_create_initial_tables/down.sql
@@ -1,9 +1,7 @@
--- migrations/{timestamp}_initial_schema/down.sql
+-- down.sql
 
 DROP TABLE IF EXISTS sale_items;
 DROP TABLE IF EXISTS sales;
-DROP TABLE IF EXISTS product_offerings;
-DROP TABLE IF EXISTS packaging_units;
 DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS users;
 
